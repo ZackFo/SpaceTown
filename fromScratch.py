82807320a51eca7f038937cb3adde3eb4fc49753
ZackFo <123978815+ZackFo@users.noreply.github.com>
2023-02-24 08:47:55 -0700

    Add files via upload

---
+++ fromScratch.py
@@ -0,0 +1,31 @@
+import pygame, sys
+from player import Player
+class Game:
+    def __init__(self):
+        player_sprite = Player((screen_width / 2, screen_height), screen_width,5)
+        self.player = pygame.sprite.GroupSingle(player_sprite)
+
+    def run(self):
+        self.player.update()
+        self.player.draw(screen)
+        #update and draw all sprite groups
+if __name__ == "__main__":
+    pygame.init()
+    screen_width = 600
+    screen_height = 600
+    screen = pygame.display.set_mode((screen_width, screen_height))
+    clock = pygame.time.Clock()
+    game = Game()
+
+    while True:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                pygame.quit()
+                sys.exit()
+
+        screen.fill((0, 0, 0))
+        game.run()
+
+        pygame.display.flip()
+        clock.tick(60)
+        #15:10 on video
